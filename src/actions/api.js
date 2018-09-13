0f0801c98b1b4b6f89ed0e85e33f08ffbb2e792e
Nishchay <nsgupta@isystango.com>
2018-09-13 16:53:43 +0530

    refactor code

--- src/actions/api.js
+++ src/actions/api.js
@@ -1,16 +1,6 @@
 import axios from 'axios';
 
-const accessToken = document.location.href.split('#')[0].split('=')[1];
-
 export default axios.create({
   withCredentials: true,
   crossDomain: true,
-  headers: {
-    "X-Shopify-Access-Token": accessToken && accessToken.toString(),
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-    // "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
-    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization, access-control-allow-origin, X-Shopify-Access-Token",
-    "Access-Control-Allow-Credentials": true
-  }
 });
--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter, Switch, Redirect} from 'react-router-dom';
-import 'react-notifications/lib/notifications.css';
+import {HashRouter, Switch } from 'react-router-dom';
 import { Route,browserHistory } from 'react-router';
 import {Provider} from 'react-redux';
 
-
-// Styles
-// Import Font Awesome Icons Set
-import 'font-awesome/css/font-awesome.min.css';
-// Import Simple Line Icons Set
-import 'simple-line-icons/css/simple-line-icons.css';
 // Import Main styles for this application
-import '../scss/style.scss';
-// Temp fix for reactstrap
-// import '../scss/core/_dropdown-menu-right.scss'
-
+ import '../scss/style.scss';
 import '../assets/style/app.scss';
 
 // scenes
